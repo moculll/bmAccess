0e09cc767e1ad71b268f4b00747c72ad51d2bf36
moculll <823309460@qq.com>
2024-12-20 19:35:55 +0800

    updated keymgr, change it as submodule

--- bmHelper/keyMgr/keyMgr.cpp
+++ bmHelper/keyMgr/keyMgr.cpp
@@ -1,19 +1,22 @@
 ﻿
-#include "keyMgr.h"
+#include "KeyMgr.h"
 #include <windows.h>
 #include <iostream>
 #include <stdio.h>
 
-
+#define DEBUG_PRINT_ENABLE 1
+#if DEBUG_PRINT_ENABLE
+#define DEBUG_PRINT(fmt, ...) \
+            do { \
+                printf("[KeyMgr.cpp][%s.%d]" fmt "\r\n", __func__, __LINE__, ##__VA_ARGS__); \
+            } while(0);
+#else
+#define DEBUG_PRINT(fmt, ...)
+#endif
 keyMgr::keyMgr() : listening(false)
 {
-
-    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc, NULL, 0);
-    if (hHook == NULL) {
-        std::cerr << "Failed to install hook!" << std::endl;
-        throw std::runtime_error("Failed to install hook");
-    }
     startListening();
+
 }
 
 keyMgr::~keyMgr()
@@ -24,11 +27,6 @@ keyMgr::~keyMgr()
     }
 }
 
-void keyMgr::bindKeys(const std::vector<int>& keys, const std::function<void(void*)>& func, void* arg)
-{
-    keyBindings[keys] = { func, arg };
-}
-
 
 LRESULT CALLBACK keyMgr::keyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -37,7 +35,6 @@ LRESULT CALLBACK keyMgr::keyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
             KBDLLHOOKSTRUCT* pKeyBoard = (KBDLLHOOKSTRUCT*)lParam;
             std::vector<int> currentKeys;
 
-            // 获取当前按下的所有修饰键
             if (GetAsyncKeyState(VK_CONTROL) & 0x8000) {
                 currentKeys.push_back(VK_CONTROL);
             }
@@ -49,11 +46,14 @@ LRESULT CALLBACK keyMgr::keyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
             }
             currentKeys.push_back(pKeyBoard->vkCode);
 
-            // 查找并执行绑定的组合键处理函数
             auto it = keyBindings.find(currentKeys);
             if (it != keyBindings.end()) {
-                const auto& keyBinding = it->second;
-                keyBinding.func(keyBinding.arg);
+                auto keyBinding = it->second;
+                auto newWork = std::async(std::launch::async, [keyBinding]() {
+                    keyBinding->invoke();
+                });
+                works.emplace_back(std::move(newWork));
+                
             }
         }
     }
@@ -61,16 +61,23 @@ LRESULT CALLBACK keyMgr::keyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 }
 
 void keyMgr::startListening() {
-   
+
     if (!listening) {
         listening = true;
-        listenerFuture = std::async(std::launch::async, []() {
+        listenerFuture = std::async(std::launch::async, [this]() {
+            hHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc, NULL, 0);
+            if (hHook == NULL) {
+                DEBUG_PRINT("KeyMgr failed to install hook, check!");
+                throw std::runtime_error("Failed to install hook");
+            }
             MSG msg;
             while (GetMessage(&msg, NULL, 0, 0)) {
                 TranslateMessage(&msg);
                 DispatchMessage(&msg);
+                
             }
         });
+        DEBUG_PRINT("KeyMgr start listening.");
     }
 }
 
@@ -81,6 +88,42 @@ void keyMgr::stopListening() {
         if (listenerFuture.valid()) {
             listenerFuture.get();
         }
-        printf("stop key");
+        if (!works.empty()) {
+            for (auto &work : works) {
+                if (work.valid()) {
+                    work.get();
+                }
+            }
+        }
+        if (listenerFuture.valid()) {
+            listenerFuture.get();
+        }
+
+
+        DEBUG_PRINT("KeyMgr stop listening.");
     }
+}
+
+int main(int argc, char* argv[])
+{
+    class test {
+    public:
+        test(int src) : insideVal(src) {}
+        static void func_0(test &obj)
+        {
+            DEBUG_PRINT("insideVal: %d", obj.insideVal);
+        }
+        static void func_1(int a, float b, double c, bool d)
+        {
+            DEBUG_PRINT("a: %d, b:%f, c:%lf, d:%d", a, b, c, d ? 1 : 0);
+        }
+    private:
+        int insideVal;
+
+    };
+    std::shared_ptr<keyMgr> keyManager = std::make_shared<keyMgr>();
+    test testClass(10086);
+    keyManager->bindKeys({ VK_F1 }, std::function(test::func_0), testClass);
+    keyManager->bindKeys({ VK_F2 }, std::function(test::func_1), 151, float(24841.614), 151.161165464, false);
+
 }
--- bmHelper/keyMgr/keyMgr.h
+++ bmHelper/keyMgr/keyMgr.h
@@ -6,26 +6,52 @@
 #include <functional>
 #include <future>
 #include <memory>
-
+#include <tuple>
 #include <windows.h>
-struct KeyBinding {
-    std::function<void(void*)> func;
-    void* arg;
+
+
+class KeyBindingBase {
+public:
+    KeyBindingBase () {}
+    ~KeyBindingBase() {}
+    virtual void invoke() = 0;
 };
+
+template <typename... Args>
+class KeyBindingWrapper : public KeyBindingBase {
+public:
+    std::function<void(Args ...)> callback;
+    std::tuple<Args...> args;
+
+    KeyBindingWrapper(std::function<void(Args ...)> srcFunc, Args&&... srcArgs) : callback(std::move(srcFunc)), args(std::forward<Args>(srcArgs)...) {}
+    inline void invoke() override
+    {
+        std::apply(callback, args);
+    }
+};
+
+
+
 class keyMgr {
 public:
     keyMgr();
     ~keyMgr();
-    void bindKeys(const std::vector<int>& keys, const std::function<void(void*)>& func, void* arg);
+    template <typename... Args>
+    inline void bindKeys(const std::vector<int>& keys, std::function<void(Args...)> func, Args&&... args) {
+        auto newKeyWrapper = std::make_shared<KeyBindingWrapper<Args...>>(std::move(func), std::forward<Args>(args)...);
+        keyBindings[keys] = std::static_pointer_cast<KeyBindingBase>(newKeyWrapper);
+    }
 private:
-    
+
 
     static LRESULT CALLBACK keyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
     void startListening();
     void stopListening();
 
     inline static HHOOK hHook = NULL;
-    inline static std::map<std::vector<int>, KeyBinding> keyBindings;
+    inline static std::map<std::vector<int>, std::shared_ptr<KeyBindingBase>> keyBindings;
     bool listening;
     std::future<void> listenerFuture;
+
+    inline static std::vector<std::future<void>> works; 
 };
